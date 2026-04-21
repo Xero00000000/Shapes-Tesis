358cb98ce8d91f75bc22c5d0fec6259672062b52
Xero0 <elias.reppalli@outlook.com>
2026-04-21 12:30:01 -0300

    dddd

--- Assets/Scripts/Systems/Abilities&Classes/AbilityData.cs
+++ Assets/Scripts/Systems/Abilities&Classes/AbilityData.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "AbilityData", menuName = "ScriptableObjects/AbilityData")]
 class AbilityData : ScriptableObject
@@ -15,12 +17,51 @@ class AbilityData : ScriptableObject
     }
 }
 
-[Serializable] abstract class AbilityEffect
+[Serializable] abstract class AbilityEffect //: MonoBehaviour
 {
     public abstract void Execute();
 }
 
 class Damage : AbilityEffect
+{
+    [SerializeField] float damageValue;
+
+    public override void Execute()
+    {
+
+    }
+
+}
+
+class TestEffect : AbilityEffect
+{
+    [SerializeField] private GameObject player;
+    public override void Execute()
+    {
+        GameObject spawnPlace = GameObject.Find("bullshitspawn");
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(spawnPlace.transform.position.x, spawnPlace.transform.position.y, spawnPlace.transform.position.z);
+
+        UnityEngine.Object.Destroy(cube, 5.0f);
+    }
+
+}
+
+class TestEffectOne : AbilityEffect
+{
+    [SerializeField] private GameObject player;
+    public override void Execute()
+    {
+        GameObject spawnPlace = GameObject.Find("bullshitspawn");
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        cube.transform.position = new Vector3(spawnPlace.transform.position.x, spawnPlace.transform.position.y, spawnPlace.transform.position.z);
+
+        UnityEngine.Object.Destroy(cube, 5.0f);
+    }
+
+}
+
+class TestEffectTwo : AbilityEffect
 {
     [SerializeField] float damageValue;
 
